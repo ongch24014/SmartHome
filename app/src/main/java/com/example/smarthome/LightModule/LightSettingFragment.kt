c952b1f9ca10ce15a89fa9af602fe4a5ba04f361
joeyelim <43492692+joeyelim@users.noreply.github.com>
2020-09-02 00:36:01 +0800

    Add Setting

--- app/src/main/java/com/example/smarthome/LightModule/LightSettingFragment.kt
+++ app/src/main/java/com/example/smarthome/LightModule/LightSettingFragment.kt
@@ -1,6 +1,7 @@
 package com.example.smarthome.LightModule
 
 
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -10,8 +11,15 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import androidx.databinding.DataBindingUtil
-import com.example.smarthome.R
 import com.example.smarthome.databinding.FragmentLightSettingBinding
+import com.google.android.material.snackbar.Snackbar
+import com.michaldrabik.classicmaterialtimepicker.CmtpDialogFragment
+import com.michaldrabik.classicmaterialtimepicker.model.CmtpTime
+import com.michaldrabik.classicmaterialtimepicker.model.CmtpTime12
+import com.michaldrabik.classicmaterialtimepicker.utilities.setOnTime12PickedListener
+import java.util.*
+
+
 
 /**
  * A simple [Fragment] subclass.
@@ -23,13 +31,32 @@ class LightSettingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        Log.i("test","456")
         val binding = DataBindingUtil.inflate<FragmentLightSettingBinding>(inflater,
-            R.layout.fragment_light_setting,container,false)
+            com.example.smarthome.R.layout.fragment_light_setting,container,false)
 
-//        val items = listOf("Material", "Design", "Components", "Android")
+//        val items = listOf("1", "2", "3", "4")
 //        val adapter = ArrayAdapter(requireContext(), R.layout.fragment_light_setting, items)
 //        (binding.txtHoursfield.editText as? AutoCompleteTextView)?.setAdapter(adapter)
+
+        binding.fabAdd.setOnClickListener { view ->
+            val timePicker = CmtpDialogFragment.newInstance()
+
+            timePicker.setInitialTime12(5, 15, CmtpTime12.PmAm.PM)
+            timePicker.setOnTime12PickedListener { time12 ->
+
+                val cal:Calendar = Calendar.getInstance()
+                val timeSetListener: TimePickerDialog.OnTimeSetListener =
+                    TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+                        cal.set(Calendar.HOUR_OF_DAY, hour)
+                        cal.set(Calendar.MINUTE, minute)
+                    }
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null)
+                    .show()
+            }
+            timePicker.show(activity!!.supportFragmentManager, "TimePickerTag")
+
+        }
 
         return binding.root
     }
