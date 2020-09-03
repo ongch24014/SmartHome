d3a3c0929f5be6dc02756c7fd827962cf38129e4
ongchunheng <ocheng.90@gmail.com>
2020-09-03 16:42:05 +0800

    history fail

--- app/src/main/java/com/example/smarthome/SmartDoorModule/DoorHistoryAdapter.kt
+++ app/src/main/java/com/example/smarthome/SmartDoorModule/DoorHistoryAdapter.kt
@@ -1,33 +1,34 @@
 package com.example.smarthome.SmartDoorModule
 
+import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.*
 import com.example.potensituitionapp.database.Door
+import com.example.smarthome.MainActivity
 import com.example.smarthome.R
-import com.example.smarthome.TextItemViewHolder
 
-class DoorHistoryAdapter (private var doorid: List<Door>):
+
+
+class DoorHistoryAdapter (private var doorid: List<Door>, var view: View?):
     RecyclerView.Adapter<DoorHistoryAdapter.ViewHolder>() {
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         val itemTitle: TextView = itemView.findViewById(R.id.txtDoorId)
-
-        init {
-            itemView.setOnClickListener{ v: View? ->  
-                val position: Int = adapterPosition
-                Toast.makeText(itemView.context,"you click on # ${position + 1}",Toast.LENGTH_SHORT).show()
-            }
-        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.item_layout,parent,false)
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
         return ViewHolder(v)
     }
 
@@ -37,9 +38,17 @@ class DoorHistoryAdapter (private var doorid: List<Door>):
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.itemTitle.text = doorid[position].doorID
+        Log.i("test", "rightere")
+
+
+        holder.itemView.setOnClickListener { v: View? ->
+            Log.i("test", "clciked")
+
+            //findNavController().navigate(R.id.action_doorHistoryFragment_to_captureFragment)
+
+        }
+
     }
-
-
 
 
 }
--- app/src/main/java/com/example/smarthome/SmartDoorModule/DoorHistoryFragment.kt
+++ app/src/main/java/com/example/smarthome/SmartDoorModule/DoorHistoryFragment.kt
@@ -2,12 +2,16 @@ package com.example.smarthome.SmartDoorModule
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.potensituitionapp.database.Door
 
 import com.example.smarthome.R
 import com.example.smarthome.database.SmartHomeDatabase
@@ -16,7 +20,7 @@ import com.example.smarthome.databinding.FragmentDoorHistoryBinding
 /**
  * A simple [Fragment] subclass.
  */
-class DoorHistoryFragment : Fragment() {
+class DoorHistoryFragment : Fragment(){
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,14 +32,14 @@ class DoorHistoryFragment : Fragment() {
         val application = requireNotNull(this.activity).application
         val dataSource = SmartHomeDatabase.getInstance(application).doorDatabaseDao
 
+        val abc = dataSource.getDoors()
+        Log.i("test",abc[1].num.toString())
+
         binding.doorList.layoutManager = LinearLayoutManager(this.context)
-        binding.doorList.adapter = DoorHistoryAdapter(dataSource.getDoors())
-
-
-
+        binding.doorList.adapter = DoorHistoryAdapter(dataSource.getDoors(),view)
 
         return binding.root
     }
 
-
 }
+
